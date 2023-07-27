37b18049bceee4bbbf67f1c2ca59057ee2c82093
TheEndIsNear <2172212+TheEndIsNear@users.noreply.github.com>
2023-07-27 07:54:30 -0600

    use the overloaded strings language extension

--- haskell/countInput/src/Lib.hs
+++ haskell/countInput/src/Lib.hs
@@ -1,12 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Lib (prompt, outputString) where
 
 import qualified Data.Text as T
 
 prompt :: T.Text
-prompt = T.pack "What is the input string? "
+prompt =  "What is the input string? "
 
 outputString :: T.Text -> T.Text
 outputString input = T.concat [input
-                              ,T.pack " has "
-                              ,T.pack $ show $ T.length input
-                              ,T.pack " characters."]
+                              ," has "
+                              ,(T.pack . show . T.length) input
+                              ," characters."]
