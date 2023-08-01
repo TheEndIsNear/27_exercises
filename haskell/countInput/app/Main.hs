621dc05e31844975b46f0702c6a2211be094a29a
TheEndIsNear <2172212+TheEndIsNear@users.noreply.github.com>
2023-07-31 21:25:20 -0600

    use a gui to display the output

--- haskell/countInput/app/Main.hs
+++ haskell/countInput/app/Main.hs
@@ -1,10 +1,34 @@
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main (main) where
 
 import Lib
-import qualified Data.Text.IO as TIO
+
+import qualified GI.Gtk as Gtk
+import Data.GI.Base
 
 main :: IO ()
 main = do
-    TIO.putStrLn prompt
-    input <- TIO.getLine
-    (TIO.putStrLn . outputString) input
+    _ <- Gtk.init Nothing
+
+    win <- new Gtk.Window [ #title := "introduction" ]
+    _ <- on win #destroy Gtk.mainQuit
+    _ <- #resize win 240 90
+
+    box <- new Gtk.Box [ #orientation := Gtk.OrientationVertical]
+    _ <- #add win box
+
+    txtField <- new Gtk.Entry []
+    _ <- #packStart box txtField False False 5
+
+    msg <- new Gtk.Label []
+    _ <- #packStart box msg True False 10
+
+    #showAll win
+
+    _ <- on txtField #activate $ do
+        entryText <- Gtk.getEntryText txtField
+        let text = outputString entryText
+        Gtk.set msg [Gtk.labelLabel := text]
+
+    Gtk.main
